b7a0d78e572f36d0998dd5ebc4f27e0af29b688d
Massimo Gervasini <mgervasini@mozilla.com>
2013-09-29 21:01:59 +0200

    fixes

--- setup/create_virtualenvs.sh
+++ setup/create_virtualenvs.sh
@@ -2,6 +2,9 @@
 # installs virtualenv
 set -e
 cd "$(dirname $0)"
+
+PYTHON="$(which python)"
+
 if [ -e "$HOME/.configuration" ]; then
     source "$HOME/.configuration"
 else
@@ -16,11 +19,13 @@ mkdir -p "$VIRTUALENV_HOME"
 TEMP_DIR="$(mktemp -d -t XXXXXXXXXX)"
 cd "$TEMP_DIR"
 echo "* downloading virtualenv (v $VIRTUALENV_VERSION)"
-curl --silent --output virtualenv.tar.gz http://pypi.python.org/packages/source/v/virtualenv/virtualenv-$VIRTUALENV_VERSION.tar.gz
+#curl --silent --output virtualenv.tar.gz http://pypi.python.org/packages/source/v/virtualenv/virtualenv-$VIRTUALENV_VERSION.tar.gz
+curl --output virtualenv.tar.gz http://pypi.python.org/packages/source/v/virtualenv/virtualenv-$VIRTUALENV_VERSION.tar.gz
+wget -O virtualenv.tar.gz http://pypi.python.org/packages/source/v/virtualenv/virtualenv-$VIRTUALENV_VERSION.tar.gz
 echo "* unpacking virtualenv"
 tar xzf virtualenv.tar.gz
 echo "* installing virtualenv"
-/usr/bin/python "virtualenv-$VIRTUALENV_VERSION/virtualenv.py" "$VIRTUALENV_HOME"
+"$PYTHON" "virtualenv-$VIRTUALENV_VERSION/virtualenv.py" "$VIRTUALENV_HOME"
 "$VIRTUALENV_HOME/bin/pip" install virtualenvwrapper
 cd /
 rm -rf "$TEMP_DIR"
